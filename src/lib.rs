9aa6fc0cc4ee0691913f9e39c81aaea7a8f6f143
Qiu Wenbo <crab2313@gmail.com>
2019-06-30 22:52:23 +0800

    impl Debug for GUIDs for better visualbility

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl std::fmt::Debug for GUID {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let data4: Vec<String> = self.Data4.iter().map(|s| format!("{:02x}", s)).collect();
+        let data4 = data4.concat();
+        write!(f, "{:08x}-{:04x}-{:04x}-{}", self.Data1 ,self.Data2, self.Data3, data4)
+    }
+}
